ceb3211a42b91d08711047febfb4244e112d1c88
RelativiBit <tjlarrechea@gmail.com>
2023-05-27 21:48:58 -0700

    CSPB-2270 Branch Commit

---
+++ CSPB 2270 - Data Structures/CSPB 2270 - Programming Assignments/CSPB 2270 - Assignment 2 - Vector10/build/CMakeFiles/run_tests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_tests.dir/code/Vector10.cpp.o"
+  "CMakeFiles/run_tests.dir/code/Vector10.cpp.o.d"
+  "CMakeFiles/run_tests.dir/tests/test_Vector10.cpp.o"
+  "CMakeFiles/run_tests.dir/tests/test_Vector10.cpp.o.d"
+  "run_tests"
+  "run_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
