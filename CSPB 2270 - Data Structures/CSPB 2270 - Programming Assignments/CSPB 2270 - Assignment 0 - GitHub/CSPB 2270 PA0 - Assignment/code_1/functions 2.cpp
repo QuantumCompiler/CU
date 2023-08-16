14ef0cb87977b2fb6a6db6339d8201b4a595ea3f
RelativiBit <tjlarrechea@gmail.com>
2023-08-16 01:42:15 -0700

    Temp Remvoal

--- CSPB 2270 - Data Structures/CSPB 2270 - Programming Assignments/CSPB 2270 - Assignment 0 - GitHub/CSPB 2270 PA0 - Assignment/code_1/functions 2.cpp
+++
@@ -1,5 +0,0 @@
-#include "functions.hpp"
-
-int fooA(){
-
-}
--- CSPB 2270 - Data Structures/CSPB 2270 - Programming Assignments/CSPB 2270 - Assignment 4 - Sorting Algorithms/CSPB 2270 PA4 - Solution/build/googletest-build/googlemock/CTestTestfile 2.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/taylor/Documents/Flurdbop/pa4-RelativiBit/build/googletest-src/googlemock
-# Build directory: /Users/taylor/Documents/Flurdbop/pa4-RelativiBit/build/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
