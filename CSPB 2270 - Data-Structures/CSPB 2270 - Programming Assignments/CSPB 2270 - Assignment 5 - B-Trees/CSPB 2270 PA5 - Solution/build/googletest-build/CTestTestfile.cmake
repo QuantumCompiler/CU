14c4b6039034571ac36b6d9baf587ee82bc9ec8f
RelativiBit <tjlarrechea@gmail.com>
2023-08-16 00:20:05 -0700

    CSPB 2270 Commit

--- CSPB 2270 - Data-Structures/CSPB 2270 - Programming Assignments/CSPB 2270 - Assignment 5 - B-Trees/CSPB 2270 PA5 - Solution/build/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/taylor/Documents/Flurdbop/pa5-RelativiBit/build/googletest-src
-# Build directory: /Users/taylor/Documents/Flurdbop/pa5-RelativiBit/build/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
