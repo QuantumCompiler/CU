11ecfc35cef658aaf88acbc0f137cf60dec1bbf4
QuantumCompiler <tjlarrechea@gmail.com>
2024-01-04 22:27:11 -0800

    Extension Clean Script Commit

---
+++ ExtClean.sh
@@ -0,0 +1,7 @@
+find . -name "*.atfi" -type f -exec rm '{}' \;
+find . -name "*.aux" -type f -exec rm '{}' \;
+find . -name "*.bcf" -type f -exec rm '{}' \;
+find . -name "*.log" -type f -exec rm '{}' \;
+find . -name "*.out" -type f -exec rm '{}' \;
+find . -name "*.ptc" -type f -exec rm '{}' \;
+find . -name "*.xml" -type f -exec rm '{}' \;
